2bc35876e73a8307f20a7219f26ea98ecfc7d306
Dhanraj <dhanrajgorantla111@gmail.com>
2023-06-09 00:22:11 +0530

    1st commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+	
+	public void method1() {
+		
+		System.out.println("Dhanraj....");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		
+		t.method1();
+		
+	
+	}
+
+}
